0dd516e32f19b77eb677ce0bfaef1b918b832f96
Tamirys Nogueira <tamirysnogueira22@hotmail.com>
2023-04-22 21:33:45 -0300

    doing token validation for user authentication and authorization

---
+++ backend/src/middleware/auth.js
@@ -0,0 +1,19 @@
+const jwt = require('jsonwebtoken')
+
+async function auth(req, res, next) {
+    const token = req.header('authorization-token')
+
+    if(!token) return res.status(403).json('Access Denied, you re not logged')
+
+    try {
+        //verifica se é o token de nossa aplicação
+        const userVerified = jwt.verify(token, process.env.TOKEN_SECRET)
+        req.user = userVerified
+
+        next()
+    } catch (error) {
+        res.status(401).json('Access denied')
+    }
+}
+
+module.exports = {auth}
