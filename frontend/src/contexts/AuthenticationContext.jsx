5d6cf4647dfe269bd4f0e13db07eaa45d7b1e036
Tamirys Nogueira <64325019+tamirysnogueira@users.noreply.github.com>
2023-04-30 21:26:38 -0300

    Update AuthenticationContext.jsx

--- frontend/src/contexts/AuthenticationContext.jsx
+++ frontend/src/contexts/AuthenticationContext.jsx
@@ -26,7 +26,7 @@ export const AuthProvider = ({ children }) => {
 
     const logout = () => {
         setTokenAdmin(``)
-        setEmailUser(``)
+        tokenUser(``)
         setEmailUser(``)
         setShouldNavigate(false)
 
